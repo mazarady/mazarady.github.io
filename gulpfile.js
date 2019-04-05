ec29af157d6857a2f7c6f993e3e64b950c40a4ea
Maaz Munir <maazamla16@gmail.com>
2019-04-04 18:25:50 -0700

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var banner = ['/*!\n',
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/creative.scss')
+  return gulp.src('scss/stylish-portfolio.scss')
     .pipe(sass())
     .pipe(header(banner, {
       pkg: pkg
@@ -31,7 +31,7 @@ gulp.task('sass', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/creative.css')
+  return gulp.src('css/stylish-portfolio.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
     }))
@@ -46,7 +46,7 @@ gulp.task('minify-css', ['sass'], function() {
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/creative.js')
+  return gulp.src('js/stylish-portfolio.js')
     .pipe(uglify())
     .pipe(header(banner, {
       pkg: pkg
@@ -74,12 +74,6 @@ gulp.task('copy', function() {
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('vendor/jquery'))
 
-  gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'))
-
-  gulp.src(['node_modules/scrollreveal/dist/*.js'])
-    .pipe(gulp.dest('vendor/scrollreveal'))
-
   gulp.src(['node_modules/jquery.easing/*.js'])
     .pipe(gulp.dest('vendor/jquery-easing'))
 
@@ -92,6 +86,9 @@ gulp.task('copy', function() {
       '!node_modules/font-awesome/*.json'
     ])
     .pipe(gulp.dest('vendor/font-awesome'))
+
+  gulp.src(['node_modules/simple-line-icons/*/*'])
+    .pipe(gulp.dest('vendor/simple-line-icons'))
 })
 
 // Default task
